1b72c5614cc6704735db732635755d437189bf2c
Aybek <idevkun@gmail.com>
2024-09-18 12:51:28 +0500

    made responsive grid menu

--- src/components/pages/waiter/acceptorder/AcceptOrder.tsx
+++ src/components/pages/waiter/acceptorder/AcceptOrder.tsx
@@ -15,12 +15,29 @@ const AcceptOrder: FC<AcceptOrderProps> = ({ className = `` }) => {
 					<TabsTrigger value="drinks">Напитки</TabsTrigger>
 				</TabsList>
 				<TabsContent value="foods">
-					<div className="grid grid-cols-2 gap-4 ">
-						<MenuCard />
-						<MenuCard />
+					<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  gap-4">
+						<MenuCard title="Пицца" price={55000} quantity={3} />
+						<MenuCard title="Сомса" price={4000} quantity={3} />
+						<MenuCard title="Сомса большой" price={8000} quantity={3} />
+						<MenuCard title="Сомса" price={4000} quantity={3} />
 					</div>
 				</TabsContent>
-				<TabsContent value="drinks">еда</TabsContent>
+				<TabsContent value="drinks">
+					<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5  gap-4">
+						<MenuCard
+							title="Кола 1.5л"
+							price={12000}
+							quantity={3}
+							image="https://dostavo4ka.uz/upload-file/2021/07/01/6218/750x750-47406b39-e089-4c49-9d6b-a46c61407b5d.jpg"
+						/>
+						<MenuCard
+							title="Фанта 1.5"
+							price={12000}
+							quantity={3}
+							image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdCcYHW9BvH1t9_CyR9f-iX9SHf-ulEp9M_A&s"
+						/>
+					</div>
+				</TabsContent>
 			</Tabs>
 		</div>
 	);
--- src/components/pages/waiter/acceptorder/menucard/MenuCard.tsx
+++ src/components/pages/waiter/acceptorder/menucard/MenuCard.tsx
@@ -1,29 +1,46 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Minus, Plus } from "lucide-react";
 import { FC } from "react";
 
 interface MenuCardProps {
 	className?: string;
+	image?: string;
+	title: string;
+	price: number;
+	quantity: number;
 }
 
-const MenuCard: FC<MenuCardProps> = ({ className = `` }) => {
+const MenuCard: FC<MenuCardProps> = ({
+	title,
+	price,
+	quantity,
+	image = "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2016/8/23/0/FNM_100116-Classic-Crust_s4x3.jpg.rend.hgtvcom.1280.720.suffix/1480972867043.webp",
+	className = ``,
+}) => {
 	return (
-		<div className={className}>
-			<div className="p-2 bg-white shadow-xl rounded-lg ">
-				<div className="mt-4 mb-1 flex justify-center">
-					<img className="object-cover h-32 w-32 rounded-full sm:h-44 sm:w-44" src="https://via.placeholder.com/300x200" alt="" />
-				</div>
-				<h4 className="text-sm font-bold text-center sm:text-lg">Avacado and Egg Toast</h4>
-				<div className="flex items-center justify-end">
-					<span className="text-xs font-extrabold text-orange-400 sm:text-sm">25 000 sum</span>
-				</div>
-			</div>
+		<div className="flex flex-col">
+			<Card className={"relative flex flex-1 flex-col max-w-xs bg-white shadow-2xl rounded-2xl"}>
+				<CardContent className="flex flex-col flex-1">
+					<img
+						src={image}
+						width={200}
+						height={200}
+						className="mb-4 mt-4 block mx-auto  self-center object-cover w-28 aspect-square sm:w-32  lg:w-36  rounded-full "
+						alt=""
+					/>
+					<h3 className="flex-1 text-xl font-extrabold">{title}</h3>
+					<p className="text-orange-500 text-sm font-bold text-left">{price.toLocaleString("Ru-ru")} сум</p>
+				</CardContent>
+				<Badge className="absolute -top-1 -right-1">{quantity}</Badge>
+			</Card>
 			<div className="flex justify-center items-center gap-4 mt-2">
-				<Button className="bg-orange-400 hover:bg-orange-200" size={"icon"}>
+				<Button className="bg-orange-500 hover:bg-orange-200 w-8 h-8" size={"icon"}>
 					<Minus />
 				</Button>
 				<span className="font-bold">3</span>
-				<Button className="bg-orange-400 hover:bg-orange-200" size={"icon"}>
+				<Button className="bg-orange-500 hover:bg-orange-200 w-8 h-8" size={"icon"}>
 					<Plus />
 				</Button>
 			</div>
