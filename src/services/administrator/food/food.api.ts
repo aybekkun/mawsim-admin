7e42720e8743033d16308434bbdd71407d06a098
Aybek <idevkun@gmail.com>
2024-12-02 16:47:31 +0500

    fix error

--- src/services/administrator/food/food.api.ts
+++ src/services/administrator/food/food.api.ts
@@ -1,6 +1,7 @@
 import { toast } from "@/hooks/use-toast";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { FoodNameService, FoodService } from "./food.service";
+import { AxiosError } from "axios";
 
 export const useGetAllFoodNameQuery = () => {
 	return useQuery({
@@ -17,8 +18,9 @@ export const useCreateFoodNameMutation = () => {
 			queryClient.invalidateQueries({ queryKey: ["foodname"] });
 			toast({ title: "Добавлено", description: "Проудукт был успешно добавлен", duration: 500 });
 		},
-		onError: (error) => {
-			toast({ title: "Create product", description: error.message, variant: "destructive", duration: 1500 });
+		onError: (error: AxiosError<any, any>) => {
+			const errorMessage = error.response?.data?.message || "An unexpected error occurred";
+			toast({ title: "Create", description: errorMessage, variant: "destructive", duration: 1500 });
 		},
 	});
 };
@@ -42,14 +44,8 @@ export const useUpdateFoodNameMutation = () => {
 	const queryClient = useQueryClient();
 
 	return useMutation({
-		mutationFn: (data: {
-			id: number;
-			name: string;
-			description: string;
-			format_id: number;
-			category_id: number;
-		}) =>
-			FoodNameService.update(data.id, data.name, data.description,  data.format_id, data.category_id),
+		mutationFn: (data: { id: number; name: string; description: string; format_id: number; category_id: number }) =>
+			FoodNameService.update(data.id, data.name, data.description, data.format_id, data.category_id),
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: ["foodname"] });
 			toast({ title: "Обнвалено", description: "Проудукт был успешно обнавлен", duration: 500 });
@@ -77,14 +73,15 @@ export const useGetOneFoodQuery = (id: number) => {
 export const useCreateFoodMutation = () => {
 	const queryClient = useQueryClient();
 	return useMutation({
-		mutationFn: (data: { quantity: number; price: number; food_id: number;}) =>
+		mutationFn: (data: { quantity: number; price: number; food_id: number }) =>
 			FoodService.create(data.food_id, data.quantity, data.price),
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: ["food"] });
 			toast({ title: "Добавлено", description: "Проудукт был успешно добавлен", duration: 500 });
 		},
-		onError: (error) => {
-			toast({ title: "Create product", description: error.message, variant: "destructive", duration: 1500 });
+		onError: (error: AxiosError<any, any>) => {
+			const errorMessage = error.response?.data?.message || "An unexpected error occurred";
+			toast({ title: "Create", description: errorMessage, variant: "destructive", duration: 1500 });
 		},
 	});
 };
