6c8c66e0ef1844e8077dd32bffb0a7cebaa90786
Aybek <idevkun@gmail.com>
2024-12-02 17:17:26 +0500

    fix

--- src/components/pages/administrator/menulist/AddMenuForm.tsx
+++ src/components/pages/administrator/menulist/AddMenuForm.tsx
@@ -30,7 +30,7 @@ const AddMenuForm: FC<AddMenuFormProps> = ({
 	const { mutate: createMenu, isPending: isCreating, isSuccess: isCreated } = useCreateMenuMutation();
 	const { mutate: updateMenu, isPending: isUpdating, isSuccess: isUpdated } = useUpdateMenuMutation();
 
-	const { data } = useGetAllFoodQuery();
+	const { data } = useGetAllFoodQuery({});
 	const transformData = data?.data.map((item) => ({
 		id: item.id,
 		value: item.name,
